1d1e797b82f486462a0ccc8b17f0c726ae9e871b
dianestephani <diane.stephani@gmail.com>
2021-01-03 21:47:54 -0800

    images added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, withSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { Container, Content } from 'native-base';
+import Swiper from 'react-native-swiper';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+//This is my stylesheet for each page, from YouTube 
+const styles = StyleSheet.create({
+  slideDefault: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +34,29 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      // <SafeAreaProvider>
+      //   <Navigation colorScheme={colorScheme} />
+      //   <StatusBar />
+      // </SafeAreaProvider>
+      <Container>
+        <Content>
+          <Swiper
+            loop = {false}
+            showsPagination = {false}
+            index = {0}
+            >
+
+            <View style = {styles.slideDefault}>
+              <Text style = {styles.text}>Camera</Text>
+            </View>
+
+            <View style = {styles.slideDefault}>
+              <Text style = {styles.text}>Chat</Text>
+            </View>
+
+          </Swiper>
+        </Content>
+      </Container>
     );
   }
 }
